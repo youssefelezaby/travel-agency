b69793cca9d2ad223c8a040039e20e5424d7a5d7
Youssef Elezaby <youssefel3zaby@gmail.com>
2025-06-04 15:10:30 +0200

    implement all users table

--- app/appwrite/auth.ts
+++ app/appwrite/auth.ts
@@ -101,3 +101,19 @@ export const getUser = async () => {
     return null;
   }
 };
+
+export const getAllUsers = async (limit: number, offset: number) => {
+  try {
+    const { documents: users, total } = await database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.limit(limit), Query.offset(offset)]
+    );
+    if (total === 0) return { users: [], total };
+
+    return { users, total };
+  } catch (e) {
+    console.log("Error fetching users");
+    return { users: [], total: 0 };
+  }
+};
